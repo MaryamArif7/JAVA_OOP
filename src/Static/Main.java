9b59371ade1c13dc11254951f00e4323b6250cae
MaryamArif7 <www.maryamarif28@gmail.com>
2024-05-25 14:23:56 +0500

    notes

--- src/Static/Main.java
+++ src/Static/Main.java
@@ -7,7 +7,8 @@ public class Main {
 //        Human kunal = new Human(22, "Kunal", 10000, false);
 //        Human rahul = new Human(34, "Rahul", 15000, true);
 //        Human arpit = new Human(34, "arpit", 15000, true);
-//
+//population is static member of Human class
+//we can acess static inside of static using clasName
 //        System.out.println(Human.population);
 //        System.out.println(Human.population);
 //        System.out.println(Human.population);
@@ -19,11 +20,14 @@ public class Main {
 
     // this is not dependent on objects
     static void fun() {
-//        greeting(); // you cant use this because it requires an instance
-        // but the function you are using it in does not depend on instances
+             //greetin is non static so that need an object we do that by using new keyword
+              // you cant use this because it requires an instance
+              // can not make a static reference to the non-static method greeting() from the type Mai
+             //  greeting();
+             // but the function you are using it in does not depend on instances
 
-        //  cannot access non static stuff without referencing their instances in
-        // a static context
+             //  cannot access non static stuff without referencing their instances in
+             // a static context
 
         //  we need  to referencing it
         Main obj = new Main();
@@ -31,12 +35,14 @@ public class Main {
     }
 
     void fun2() {
+        //can acess the non static inside of non static 
         greeting();
     }
 
     //  something which is not static, belongs to an object
     void greeting() {
-//        fun();
+        //fun() isa static method to acess fun which is static inside of non static we can call it
+       // fun();
         System.out.println("Hello world");
     }
 }
